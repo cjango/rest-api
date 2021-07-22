9e5922314acb7074ec43ab34ca979cc950cbe77f
Jason <chenjxlg@163.com>
2021-07-22 17:27:08 +0800

    update

--- config/rest.php
+++ config/rest.php
@@ -2,17 +2,26 @@
 
 return [
 
-    'guard' => env('REST_API_GUARD', 'passport'),
+    'guard'                         => env('REST_API_GUARD', 'passport'),
 
-    'tokens_expire_time'            => 1,
-    'refresh_tokens_expire'         => 10,
-    'personal_access_tokens_expire' => 10,
+    /**
+     * TOKEN 过期时间，这个不确定是啥
+     */
+    'tokens_expire_time'            => env('tokens_expire_time', 60),
+    /**
+     * 可用的刷新时间
+     */
+    'refresh_tokens_expire'         => env('refresh_tokens_expire', 60 * 24 * 7),
+    /**
+     * 个人TOKEN过期时间
+     */
+    'personal_access_tokens_expire' => env('personal_access_tokens_expire', 60),
 
     /**
      * Passport Cache Config
      */
     'passport_cache'                => [
-        'enable'     => env('REST_PASSPORT_CACHE', false),
+        'enable'     => env('REST_PASSPORT_CACHE', true),
         // Cache key prefix
         'prefix'     => 'passport_',
         // The lifetime of token cache,
